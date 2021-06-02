6ec38ce282d4eff931d3ced4fc6a606420ee1e3d
papercodes-ia <84306850+papercodes-ia@users.noreply.github.com>
2021-06-02 09:31:49 +0800

    Add files via upload

---
+++ configs/kitti/baseline_FoveaBox.py
@@ -0,0 +1,18 @@
+_base_ = './fovea_r50_fpn_4x4_1x.py'
+model = dict(
+    bbox_head=dict(
+        num_classes=1,
+        with_deform=True,
+        norm_cfg=dict(type='GN', num_groups=32, requires_grad=True)),
+    test_cfg=dict(nms=dict(type='nms', iou_threshold=0.7)))
+# learning policy
+runner = dict(type='EpochBasedRunner', max_epochs=12)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+evaluation = dict(interval=1, metric='bbox')
+optimizer = dict(lr=0.005)
+lr_config = dict(warmup='linear', 
+                 warmup_ratio=1.0 / 3,
+                 step=[8, 11])
+checkpoint_config = dict(interval=1)
+work_dir = 'work_dirs/kitti/baseline_FoveaBox'
